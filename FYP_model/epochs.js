bd28ba0782e00b423448d3099bd59e862b493aeb
Matthew <Matthewhungwaichung@gmail.com>
2023-04-11 06:24:43 +0800

    update

--- FYP_model/epochs.js
+++ FYP_model/epochs.js
@@ -8,14 +8,11 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
     return;
   }
   
-  // Parse the JSON data
   const json_data = JSON.parse(data);
   
-  // Use the JSON data
-  const inputDim = 3;  // Number of input features
-  const numClasses = 3;  // Number of output classes (multi-class classification)
+  const inputDim = 3; 
+  const numClasses = 3; 
 
-  // Define the architecture of the model
   const model = tf.sequential();
   model.add(tf.layers.dense({
     inputShape: [inputDim],
@@ -24,18 +21,16 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
   }));
   model.add(tf.layers.dense({
     units: numClasses,
-    activation: 'softmax'  // Use softmax activation for multi-class classification
+    activation: 'softmax'  
   }));
 
-  // Compile the model with an optimizer and loss function
   const optimizer = tf.train.adam();
   model.compile({
     optimizer: optimizer,
-    loss: 'categoricalCrossentropy',  // Use categorical cross-entropy loss for multi-class classification
+    loss: 'categoricalCrossentropy', 
     metrics: ['accuracy']
   });
 
-  // Define your own input and output data
   const trainIn = json_data.ti
   const trainOut = json_data.to
   const testIn = json_data.tei
@@ -43,7 +38,6 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
   const input = tf.tensor2d(trainIn);
   const output = tf.tensor2d(trainOut);
 
-  // Define an async function to train the model for a given number of epochs
   async function trainModelForEpochs(epochs) {
     await model.fit(input, output, {
       batchSize: 32,
@@ -58,7 +52,6 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
     console.log(`Model for ${epochs} epochs saved!`);
   }
 
-  // Loop over the epoch values and train the model for each value
   const epochValues = Array.from({ length: 20 }, (_, i) => i + 1);
   for (const epochValue of epochValues) {
     await trainModelForEpochs(epochValue);
--- FYP_model/matrix.js
+++ FYP_model/matrix.js
@@ -8,14 +8,11 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
     return;
   }
   
-  // Parse the JSON data
   const json_data = JSON.parse(data);
   
-  // Use the JSON data
-  const inputDim = 3;  // Number of input features
-  const numClasses = 3;  // Number of output classes (multi-class classification)
+  const inputDim = 3;  
+  const numClasses = 3;  
 
-  // Define the architecture of the model
   const model = tf.sequential();
   model.add(tf.layers.dense({
     inputShape: [inputDim],
@@ -24,18 +21,16 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
   }));
   model.add(tf.layers.dense({
     units: numClasses,
-    activation: 'softmax'  // Use softmax activation for multi-class classification
+    activation: 'softmax' 
   }));
 
-  // Compile the model with an optimizer and loss function
   const optimizer = tf.train.adam();
   model.compile({
     optimizer: optimizer,
-    loss: 'categoricalCrossentropy',  // Use categorical cross-entropy loss for multi-class classification
+    loss: 'categoricalCrossentropy',  
     metrics: ['accuracy']
   });
 
-  // Define your own input and output data
   const trainIn = json_data.ti
   const trainOut = json_data.to
   const testIn = json_data.tei
@@ -43,34 +38,28 @@ fs.readFile('data.json', 'utf8', async (err, data) => {
   const input = tf.tensor2d(trainIn);
   const output = tf.tensor2d(trainOut);
 
-  // Train the model on the training data
   await model.fit(input, output, {
     batchSize: 32,
     epochs: 15
   });
 
-  // Evaluate the model on the test set
   const evalInput = tf.tensor2d(testIn);
   const evalOutput = tf.tensor2d(TestOut);
   const evalResult = model.evaluate(evalInput, evalOutput);
   console.log(`Test accuracy: ${evalResult[1]}`);
 
-  // Predict the classes for the test set
   const predictions = model.predict(evalInput);
   const predictedClasses = predictions.argMax(-1).dataSync();
   const trueClasses = evalOutput.argMax(-1).dataSync();
 
-  // Compute the confusion matrix
   const confusionMatrix = Array.from({ length: numClasses }, () => Array.from({ length: numClasses }, () => 0));
   for (let i = 0; i < trueClasses.length; i++) {
     confusionMatrix[trueClasses[i]][predictedClasses[i]]++;
   }
 
-  // Print the confusion matrix
   console.log('Confusion matrix:');
   console.table(confusionMatrix);
 
-  // Save the model
   await model.save('file://./my_model');
   console.log('Model saved!');
 });
